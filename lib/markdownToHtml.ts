dc21f7c9ac1ea8a7b27e43ad9b11e85ed2659067
Kim Dong Hun <hunkim98@gmail.com>
2022-08-23 01:57:09 +0900

    Add image to post

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,16 @@
-import { remark } from 'remark'
-import html from 'remark-html'
+import { remark } from "remark";
+import html from "remark-html";
+import parse from "remark-parse";
+import prism from "remark-prism";
+import remarkImages from "remark-images";
+import { unified } from "unified";
+import remarkUnwrapImages from "remark-unwrap-images";
 
-export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
-  return result.toString()
+export default function markdownToHtml(markdown: string) {
+  const result = remark()
+    .use(parse)
+    .use(html, { sanitize: false })
+    .use(prism)
+    .processSync(markdown);
+  return String(result);
 }
