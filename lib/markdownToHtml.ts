3892a1559f2c11bfd5c525029f52d982d39a85ed
Kim Dong Hun <hunkim98@gmail.com>
2022-08-23 11:57:05 +0900

    Add style for highlighting code

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -5,12 +5,13 @@ import prism from "remark-prism";
 import remarkImages from "remark-images";
 import { unified } from "unified";
 import remarkUnwrapImages from "remark-unwrap-images";
+import remarkHighlightjs from "remark-highlight.js";
 
 export default function markdownToHtml(markdown: string) {
   const result = remark()
     .use(parse)
     .use(html, { sanitize: false })
-    .use(prism)
+    .use(prism, { plugins: ["line-numbers"] })
     .processSync(markdown);
   return String(result);
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { AppProps } from "next/app";
 import "../styles/index.css";
+import "prismjs/themes/prism-tomorrow.css";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
