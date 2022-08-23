9eb992482d9160533e9a5eb2909cad28b06c8b0d
Kim Dong Hun <hunkim98@gmail.com>
2022-08-23 14:15:13 +0900

    Modify g tag placement

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,30 @@ import { AppProps } from "next/app";
 import "../styles/index.css";
 import "prismjs/themes/prism-tomorrow.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {process.env.RUNTIME_ENV !== "development" && (
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+      )}
+      {process.env.RUNTIME_ENV !== "development" && (
+        <Script strategy="lazyOnload">
+          {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+      `}
+        </Script>
+      )}
+      <Component {...pageProps} />
+    </>
+  );
 }
