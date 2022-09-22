623e6f8d99ad46024ee0d5d971510d56fdf31856
Kim Dong Hun <hunkim98@gmail.com>
2022-09-23 01:08:56 +0900

    update package.json to have export

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    IMP_UID: process.env.IMP_UID,
-    CLIENT_URL: process.env.CLIENT_URL,
     RUNTIME_ENV: process.env.RUNTIME_ENV,
     // GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   },
