af88df6564b3bfb39ea00eeb9b6be2a02a6cf00b
Kim Dong Hun <hunkim98@gmail.com>
2023-05-09 15:58:48 +0900

    Add style for blockquote and change theme color

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -2,6 +2,7 @@ import { remark } from "remark";
 import html from "remark-html";
 import parse from "remark-parse";
 import prism from "remark-prism";
+import remark2rehype from "remark-rehype";
 import remarkImages from "remark-images";
 import { unified } from "unified";
 import remarkUnwrapImages from "remark-unwrap-images";
@@ -12,6 +13,7 @@ export default function markdownToHtml(markdown: string) {
     .use(parse)
     .use(html, { sanitize: false })
     .use(prism, { plugins: ["line-numbers"] })
+    .use(remarkHighlightjs)
     .processSync(markdown);
   return String(result);
 }
