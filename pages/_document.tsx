07b9cd786379c3f64ae1992d02f71e5175a5a783
Kim Dong Hun <hunkim98@gmail.com>
2023-07-31 14:15:38 +0900

    Remove uppermost head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,14 +6,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <meta
-            property="og:image"
-            content={`assets/profile/dotting_intro.JPG`}
-            key="ogimage"
-          />
-          <meta property="og:title" content="픽글 Pickgeul!" key="ogtitle" />
-        </Head>
+        <Head />
         <body className="bg-[#F1F1F1]">
           <Main />
           <NextScript />
