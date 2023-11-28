93022b8bfd02da541ebf10e50dbf16207c738716
Donghun Kim <hunkim98@gmail.com>
2023-11-28 17:52:24 +0900

    Update constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 export const EXAMPLE_PATH = "blog-starter";
 export const CMS_NAME = "Markdown";
 export const HOME_OG_IMAGE_URL =
-  "https://hunkim98.github.io/assets/profile/cover_image.png";
-export const BLOG_URL = "https://hunkim98.github.io";
+  "https://donghunkim.dev/assets/profile/cover_image.png";
+export const BLOG_URL = "https://donghunkim.dev";
