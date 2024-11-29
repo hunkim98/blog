40bf3aa0b3cfb6619cf98d4fa65fd60008a72b84
Hun Kim <hunkim98@gmail.com>
2024-11-29 04:47:21 -0500

    Run unoptimized image

--- next.config.js
+++ next.config.js
@@ -7,12 +7,17 @@ const nextConfig = {
   //   loader: "akamai",
   //   path: "",
   // },
+  output: 'export',
   reactStrictMode: true,
   env: {
     // remember that you should update env in .github/workflow/gh-pages.deploy.yml
     NEXT_PUBLIC_RUNTIME_ENV: process.env.NEXT_PUBLIC_RUNTIME_ENV,
     NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
     // GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  },
+
+  images: {
+    unoptimized: true,
   },
   webpack: (config, options) => {
     config.module.rules.push({
