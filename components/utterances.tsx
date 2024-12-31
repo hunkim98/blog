243530724e3f6323bf6b28f434167e950a69a9d1
Hun Kim <hunkim98@gmail.com>
2024-12-31 13:17:51 -0500

    Fix utterance duplicate

--- components/utterances.tsx
+++ components/utterances.tsx
@@ -1,23 +1,43 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
-const Utterances: React.FC = () => (
-  <section
-    ref={(elem) => {
-      if (!elem) {
-        return
-      }
-      const scriptElem = document.createElement('script')
-      scriptElem.src = 'https://utteranc.es/client.js'
-      scriptElem.async = true
-      scriptElem.setAttribute('repo', 'hunkim98/hunkim98.github.io')
-      scriptElem.setAttribute('issue-term', 'title')
-      scriptElem.setAttribute('theme', 'github-dark')
-      scriptElem.setAttribute('label', 'blog-comment')
-      scriptElem.crossOrigin = 'anonymous'
-      elem.appendChild(scriptElem)
-    }}
-  />
-)
+const Utterances: React.FC = () => {
+  const ref = useRef<HTMLDivElement>()
+
+  useEffect(() => {
+    if (!ref.current) {
+      return
+    }
+    const scriptElem = document.createElement('script')
+    scriptElem.src = 'https://utteranc.es/client.js'
+    scriptElem.async = true
+    scriptElem.setAttribute('repo', 'hunkim98/hunkim98.github.io')
+    scriptElem.setAttribute('issue-term', 'title')
+    scriptElem.setAttribute('theme', 'github-dark')
+    scriptElem.setAttribute('label', 'blog-comment')
+    scriptElem.crossOrigin = 'anonymous'
+
+    ref.current.appendChild(scriptElem)
+  }, [])
+  return (
+    <section
+      ref={ref}
+      //   if (!elem) {
+      //     return
+      //   }
+      //   const scriptElem = document.createElement('script')
+      //   scriptElem.src = 'https://utteranc.es/client.js'
+      //   scriptElem.async = true
+      //   scriptElem.setAttribute('repo', 'hunkim98/hunkim98.github.io')
+      //   scriptElem.setAttribute('issue-term', 'title')
+      //   scriptElem.setAttribute('theme', 'github-dark')
+      //   scriptElem.setAttribute('label', 'blog-comment')
+      //   scriptElem.crossOrigin = 'anonymous'
+      //   // check if utterance is already loaded
+      //   elem.appendChild(scriptElem)
+      // }}
+    />
+  )
+}
 
 {
   /* <script src="https://utteranc.es/client.js"
